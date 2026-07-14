71d4c88e0d7d6c0d342c9df332d67aa565ff2923
Ty Dwyer <ty@Mac.hsd1.il.comcast.net>
2026-07-13 22:44:07 -0500

    created packet entity and some boilerplate

---
+++ src/main/java/com/example/networktraffic/NetworktrafficApplication.java
@@ -0,0 +1,13 @@
+package com.example.networktraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworktrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworktrafficApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/networktraffic/NetworktrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.networktraffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworktrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
