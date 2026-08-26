fc6f3fe864e8380a2a7502dddcac7eb8568215b0
ty-dwyer <92275990+ty-dwyer@users.noreply.github.com>
2026-08-26 18:05:00 -0500

    added pcap file upload

--- src/main/java/com/example/networktraffic/controllers/PacketController.java
+++ src/main/java/com/example/networktraffic/controllers/PacketController.java
@@ -1,5 +1,6 @@
 package com.example.networktraffic.controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.networktraffic.entities.Alert;
 import com.example.networktraffic.entities.Packet;
@@ -41,4 +43,17 @@ public class PacketController {
   public List<Alert> getAllAlerts(){
     return alertRepository.findAll();
   }
+
+@PostMapping("/ingest/upload")
+public String uploadAndIngest(@RequestParam("file") MultipartFile file) throws IOException {
+    File tempFile = File.createTempFile("upload-", ".pcap");
+    file.transferTo(tempFile);
+
+    int count = ingestionService.ingest(tempFile.getAbsolutePath());
+
+    tempFile.delete();
+
+    return count + " packets ingested";
+}
+
 }
